7b8167ceae2d9b439331685132b9c054feb564d5
LukeDev42 <ldlstarwars@gmail.com>
2021-09-08 15:20:24 -0400

    Added createObject to MainActivity.java
    -debug statements added to follow button press though the classes following MVC

--- app/src/main/java/com/ldleiter/homequartermaster/MainActivity.java
+++ app/src/main/java/com/ldleiter/homequartermaster/MainActivity.java
@@ -3,6 +3,10 @@ package com.ldleiter.homequartermaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void createObject(View view)
+    {
+        ObjectType ot = new ObjectType("Car", new Date());
+        Log.i("CreateObjectButton", "This button created an object");
+    };
 }
--- app/src/main/java/com/ldleiter/homequartermaster/ObjectType.java
+++ app/src/main/java/com/ldleiter/homequartermaster/ObjectType.java
@@ -1,11 +1,13 @@
 package com.ldleiter.homequartermaster;
 
+import android.util.Log;
+
 import java.util.Date;
 
 public class ObjectType {
 
     public ObjectType (String objectName, Date goodUntil) {
-        System.out.println("Create new object type");
+        Log.i("CreateObjectConstructor", "Create new object type");
         this.objectName = objectName;
         this.goodUntil = goodUntil;
 
