2bcd274cf0f2a584f89e76110c21d1c42dc1ca9a
LukeDev42 <ldlstarwars@gmail.com>
2021-09-08 14:41:24 -0400

    Created ObjectType Constructor
    -has two parameters

--- app/src/main/java/com/ldleiter/homequartermaster/ObjectType.java
+++ app/src/main/java/com/ldleiter/homequartermaster/ObjectType.java
@@ -1,7 +1,16 @@
 package com.ldleiter.homequartermaster;
 
+import java.util.Date;
+
 public class ObjectType {
 
+    public ObjectType (String objectName, Date goodUntil) {
+        System.out.println("Create new object type");
+        this.objectName = objectName;
+        this.goodUntil = goodUntil;
+
+    };
     private String objectName;
+    private Date goodUntil;
 
 }
