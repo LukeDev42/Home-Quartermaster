9e5e70521e3012354ac29bb6baae879e71dbd75c
LukeDev42 <ldlstarwars@gmail.com>
2021-12-17 00:03:42 -0500

    App now has landscape layouts

--- app/src/main/java/com/ldleiter/homequartermaster/MainActivity.java
+++ app/src/main/java/com/ldleiter/homequartermaster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ldleiter.homequartermaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -10,6 +11,8 @@ import androidx.navigation.ui.NavigationUI;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.List;
@@ -36,6 +39,28 @@ public class MainActivity extends AppCompatActivity
                     Builder(navController.getGraph()).build();
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        if(item.getItemId() == R.id.settings)
+        {
+            Log.d("App Bar Menu", "The settings menu was selected");
+            return true;
+        }
+        else
+        {
+            Log.d("App Bar Menu", "Nothing was selected");
+            return false;
+        }
+
     }
 
     @Override
